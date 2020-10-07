3d6d387341f32924b03b2b1fce030a139ec415bb
younj1601 <younj1601@wrdsb.ca>
2020-10-07 13:31:58 +0000

    Finished the for loop summative

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *Calculates the sum of the odd numbers upto the entered number
+ * @author Jackson
  */
 public class Main {
 
@@ -9,7 +10,25 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
-  }
+    //Scanner for input
+    Scanner input = new Scanner(System.in);
+
+    //get the interger from the user
+    System.out.println("Please enter the maximum number to sum to:");
+    int num = input.nextInt();
+
+    //declare and initialize sum variable
+    int sum = 0;
+
+    //for loop to count to the inputted number
+    for(int count = 0; count <= num; count++){
+      //if statement to check if counted number is odd or not
+      if(count % 2 == 1){
+        //storing the odd number into a another variable for later use
+        sum = sum + count;
+      }
+    }
+    //telling the user their answer
+    System.out.println("The sum of the odd numbers is " + sum);
+}
 }
